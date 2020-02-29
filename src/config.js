a1d7623e3e7eec9b4d0c87af504a1c429ee5c12f
yuval <yuvalzernik.yz@@gmail.com>
2020-02-29 19:29:49 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "examplet-bucket-notes"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://ks3vbxxwh6.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_FGR1IyZNU",
+      APP_CLIENT_ID: "1up0qt8af8qn790lll7ov4nr91",
+      IDENTITY_POOL_ID: "us-east-2:96d829f0-58f6-4263-8e40-0eb4593ff757"
+    }
+  };
